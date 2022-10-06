03b7748bcee0c0716c8ebf2347b5f989c3deb18f
Ali Hasan <110688357+Alihasanlitonislam@users.noreply.github.com>
2022-10-06 21:51:14 +0600

    Update setTime.js

--- setTime.js
+++ setTime.js
@@ -10,3 +10,14 @@ function getData(words){
 getData(function(data){
   console.log(data)
 })
+//map toiri korar system
+const options = ["name", "phone", "email", "address"]
+const pitlu = (array, callback = function () { }) => {
+    let data = []
+    array.forEach(x => {
+        const y = callback(x)
+        data.push(y)
+    })
+    return data
+}
+console.log(pitlu(options, x => x.toUpperCase()))
