766ae419405c74118f7ab793c2c1a3d68cb1fe43
Ali Hasan <110688357+Alihasanlitonislam@users.noreply.github.com>
2022-10-06 19:23:21 +0600

    Update setTime.js

--- setTime.js
+++ setTime.js
@@ -1,3 +1,12 @@
 setTimeout(function(){
   console.log(" Ali hasan liton islam")
 },3000)
+//setTimeout 2
+function getData(words){
+  setTimeout(function(){
+    words(" i am learn progrem in javascript")
+  },3000)
+}
+getData(function(data){
+  console.log(data)
+})
